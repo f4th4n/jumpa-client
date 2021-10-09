7c72c9b556aa239321948efcb2e0b7d122657072
Wildan Fathan <wilfathan@gmail.com>
2021-10-09 19:26:43 +0700

    Init web

--- src/helper.js
+++
@@ -1,3 +0,0 @@
-export default {
-	frameSchedule: 60 / 1000
-}
---
+++ web/src/index.js
@@ -0,0 +1,5 @@
+import { Socket } from 'phoenix'
+import _ from 'lodash'
+
+const test = _.join(['Hello', 'webpack'], ' ');
+console.log('ffff', test)
