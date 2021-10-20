7388f6cfaa836c7ada3163ee15341736b184ba5a
Wildan Fathan <wilfathan@gmail.com>
2021-10-20 09:30:49 +0700

    Add documentation to build to unity

--- web/README.md
+++ web/README.md
@@ -10,3 +10,8 @@ http://localhost:3000/?player_token=your-token
 Example
 http://localhost:3000/?player_token=gg
 ```
+
+### Deploy to Unity
+Change isProd to true in config.json
+$ npx webpack
+copy ./dist/main.js to unity build
--- web/src/index.js
+++ web/src/index.js
@@ -2,15 +2,18 @@ import { initializeState } from './initialize-state'
 import { socket } from './channels/index'
 import { renderPlayer } from './players/render-player'
 import { global } from './global'
+import config from './config.json'
 
 initializeState
   .setState()
   .then(() => {
-    renderPlayer.start()
+    if (!config.isProd) {
+      renderPlayer.start()
+    }
     socket.start()
     global.start()
   })
   .catch((e) => {
     console.log('e', e)
-		//alert(e)
+    //alert(e)
   })
